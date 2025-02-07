29c39f5540724d2307814be8e84f5b9f89d10c74
anisharadhey <136369506+anisharadhey@users.noreply.github.com>
2025-02-07 16:03:17 +0000

    Added cmast file and created

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,15 @@ from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+    page_title='Temperature dashboard',
+    page_icon=':fire:', # This is an emoji shortcode. Could be a URL too.
 )
 
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
 
 @st.cache_data
+
 def get_gdp_data():
     """Grab GDP data from a CSV file.
 
@@ -57,6 +58,50 @@ def get_gdp_data():
 
     return gdp_df
 
+@st.cache_data
+
+def get_temp_data():
+    """Grab temperature data from a CSV file.
+
+    This uses caching to avoid having to read the file every time.
+    """
+
+    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
+    DATA_FILENAME = Path(__file__).parent/'data/AverageCMASTtemp.csv'
+    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+
+    MIN_YEAR = 1960
+    MAX_YEAR = 2022
+
+    # The data above has columns like:
+    # - Country Name
+    # - Country Code
+    # - [Stuff I don't care about]
+    # - GDP for 1960
+    # - GDP for 1961
+    # - GDP for 1962
+    # - ...
+    # - GDP for 2022
+    #
+    # ...but I want this instead:
+    # - Country Name
+    # - Country Code
+    # - Year
+    # - GDP
+    #
+    # So let's pivot all those year-columns into two: Year and GDP
+    gdp_df = raw_gdp_df.melt(
+        ['Country Code'],
+        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
+        'Year',
+        'GDP',
+    )
+
+    # Convert years from string to integers
+    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+
+    return gdp_df
+
 gdp_df = get_gdp_data()
 
 # -----------------------------------------------------------------------------
@@ -64,11 +109,9 @@ gdp_df = get_gdp_data()
 
 # Set the title that appears at the top of the page.
 '''
-# :earth_americas: GDP dashboard
+# :fire: Temperature dashboard
 
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
+Browse water temperature data collected by each sensor at DUML Aquafarm and CMAST oyster farm between April and October 2024.
 '''
 
 # Add some spacing
@@ -92,7 +135,7 @@ if not len(countries):
 selected_countries = st.multiselect(
     'Which countries would you like to view?',
     countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
+    ['USA', 'MEX', 'JPN'])
 
 ''
 ''
@@ -105,7 +148,7 @@ filtered_gdp_df = gdp_df[
     & (from_year <= gdp_df['Year'])
 ]
 
-st.header('GDP over time', divider='gray')
+st.header('Water temperature over time', divider='gray')
 
 ''
 
@@ -119,33 +162,33 @@ st.line_chart(
 ''
 ''
 
+# Remove bottom part
+# first_year = gdp_df[gdp_df['Year'] == from_year]
+# last_year = gdp_df[gdp_df['Year'] == to_year]
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+# st.header(f'GDP in {to_year}', divider='gray')
 
-st.header(f'GDP in {to_year}', divider='gray')
+# ''
 
-''
+# cols = st.columns(4)
 
-cols = st.columns(4)
+# for i, country in enumerate(selected_countries):
+#     col = cols[i % len(cols)]
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
+#     with col:
+#         first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+#         last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
 
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+#         if math.isnan(first_gdp):
+#             growth = 'n/a'
+#             delta_color = 'off'
+#         else:
+#             growth = f'{last_gdp / first_gdp:,.2f}x'
+#             delta_color = 'normal'
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+#         st.metric(
+#             label=f'{country} GDP',
+#             value=f'{last_gdp:,.0f}B',
+#             delta=growth,
+#             delta_color=delta_color
+#         )
