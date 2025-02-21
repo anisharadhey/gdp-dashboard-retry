110f1a32ce71b2e89e7cefba0c66966e1b1213ff
anisharadhey <136369506+anisharadhey@users.noreply.github.com>
2025-02-21 14:47:15 +0000

    Tried side-by-side plots for plotting survivorship!

--- pages/survivorship.py
+++ pages/survivorship.py
@@ -4,63 +4,50 @@ import pandas as pd
 import math
 from pathlib import Path
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='Temperature dashboard',
-    page_icon=':fire:', # This is an emoji shortcode. Could be a URL too.
-)
-
 @st.cache_data
 
-def get_temp_data():
-    """Grab temperature data from a CSV file.
+def get_survivor_data():
+    """Grab survivorship data from a CSV file.
 
     This uses caching to avoid having to read the file every time.
     """
 
     # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/AverageCMASTtemp.csv'
-    temp_df = pd.read_csv(DATA_FILENAME)
+    DATA_FILENAME = Path(__file__).parent/'OysterMortalityData_Processed.csv'
+    surv_df = pd.read_csv(DATA_FILENAME)
+
+    # Remove empty columns and rows from the read-in CSV file
+    surv_df = surv_df.dropna(axis = 1, how = 'all').dropna(axis = 0, how = 'all')
 
     # Convert times from string to datetime objects
-    temp_df['Time'] = pd.to_datetime(temp_df['Time'], format = 'mixed')
+    surv_df['Date'] = pd.to_datetime(surv_df['Date'], format='%m/%d/%y')
     
-    # Make colymns for month, day and time
-    temp_df['Month'] = [dt.month for dt in temp_df['Time']]
-    temp_df['Day'] = [dt.day for dt in temp_df['Time']]
-    temp_df['Hour'] = [dt.hour for dt in temp_df['Time']]
-
-    # Get only rows for data in May
-    # temp_df = temp_df[(temp_df['Month'] == 5)]
-
-    # Get only rows for May 14
-    # temp_df = temp_df[(temp_df['Day'] == 14)]
-
-    return temp_df
+    return surv_df
 
 # gdp_df = get_gdp_data()
-temp_df = get_temp_data()
+surv_df = get_survivor_data()
 
-# temp_df
+# Looks great!
+# surv_df
 
 # -----------------------------------------------------------------------------
 # Draw the actual page
 
 # Set the title that appears at the top of the page.
 '''
-# :fire: Temperature dashboard
+# :oyster: Survivorship dashboard
 
-Browse water temperature data collected at the CMAST oyster farm between May and October 2024.
+Browse water oyster survivorship curves from CMAST oyster farm between May and October 2024.
 '''
 
 ''
 
 # Make a slider based on times
-min_time = temp_df['Time'].min().to_pydatetime()
-max_time = temp_df['Time'].max().to_pydatetime()
+min_time = surv_df['Date'].min().to_pydatetime()
+max_time = surv_df['Date'].max().to_pydatetime()
 
 from_time, to_time = st.slider(
-    'Which hours are you interested in?',
+    'Which days are you interested in?',
     min_value = min_time,
     max_value = max_time,
     value=[min_time, max_time],
@@ -68,34 +55,65 @@ from_time, to_time = st.slider(
 
 ''
 
-sensors = temp_df['Location'].unique()
+strains = surv_df['Strain'].unique()
 
-if not len(sensors):
-    st.warning("Select at least one sensor")
+if not len(strains):
+    st.warning("Select at least one genetic strain")
 
-selected_sensors = st.multiselect(
-    'Which sensors would you like to view?',
-    sensors,
-    ['Array', 'Line 1A', 'Line 1B'],
+selected_strains = st.multiselect(
+    'Which genetic strains would you like to view?',
+    strains,
+   ['CS', 'SJ'],
     key = 3
     )
 
-''
-
 # Filter the data
-filtered_temp_df = temp_df[
-    (temp_df['Location'].isin(selected_sensors))
-    & (temp_df['Time'] <= to_time)
-    & (from_time <= temp_df['Time'])
+filtered_surv_df = surv_df[
+    (surv_df['Strain'].isin(selected_strains))
+    & (surv_df['Date'] <= to_time)
+    & (from_time <= surv_df['Date'])
 ]
 
-st.header('Water temperature over time', divider='gray')
+# Set up to columns for displaying line graphs
+col1, col2 = st.columns([0.5, 0.5])
 
-''
+#SELECTION 1 COLUMN
+with col1:
 
-st.line_chart(
-    filtered_temp_df,
-    x='Time',
-    y='TempC',
-    color='Location',
-)
+    st.header('Survivorship fraction at DUML', divider='gray')
+
+    ''
+
+    # Get just DUML data
+    DUML_data = filtered_surv_df[(filtered_surv_df['Site'] == 'DUML')]
+
+    # DUML_data
+
+    st.line_chart(
+        DUML_data,
+        x = 'Date',
+        y = 'Survivorship_ActualStartingDensityBase_PreventFranken',
+        color = 'BagNumber',
+        y_label = 'Survivorship fraction'
+        )
+   
+
+#SELECTION 2 COLUMN
+with col2:
+
+    st.header('Survivorship fraction at CMAST', divider='gray')
+
+    ''
+
+    # Get just CMAST data
+    CMAST_data = filtered_surv_df[(filtered_surv_df['Site'] == 'CMAST')]
+    
+    # CMAST_data
+
+    st.line_chart(
+        CMAST_data,
+        x = 'Date',
+        y = 'Survivorship_ActualStartingDensityBase_PreventFranken',
+        color = 'BagNumber',
+        y_label = 'Survivorship fraction'
+        )
